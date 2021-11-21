b7b563fa2849ea0a529cf648476ec28d9009970c
EHOT125 <Kochitov90work@mail.ru>
2021-11-21 21:31:48 +1000

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import model.Cat;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -5,8 +6,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(cat == cat1);
+        System.out.println(bean == bean1);
     }
 }
